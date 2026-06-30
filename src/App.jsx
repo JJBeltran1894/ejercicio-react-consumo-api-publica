667aad064a9e9993eb58c824d6c853a65deadec4
JJBeltran1894 <juanjbeltran94@hotmail.com>
2026-06-29 21:28:58 -0500

    JJBA: Se modifica el archivo App.jsx que realiza la consulta mediante  funcion asincrona y renderizar las cards de cada personaje

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,38 @@
 import { useState } from "react";
-
+import { useEffect } from "react";
+import PersonajeCard from "./components/PersonajeCard";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [simpsons, setSimpsons] = useState([]);
+  useEffect(() => {
+    const obtenerSimpsons = async () => {
+      try {
+        const response = await fetch(
+          "https://thesimpsonsapi.com/api/characters?page=1",
+        );
+        const datos = await response.json();
+        setSimpsons(datos.results);
+      } catch (error) {
+        console.error("Error al obtener los personajes:", error);
+      }
+    };
+    obtenerSimpsons();
+  }, []);
 
-  return 0;
+  return (
+    <div className="app-container">
+      <header className="app-header">
+        <h1 className="app-title">The Simpsons API</h1>
+      </header>
+
+      <div className="grid-container">
+        {simpsons.map((personaje) => (
+          <PersonajeCard key={personaje.id} personaje={personaje} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default App;
